bbb73f168f6f4bd0f378c0b533060f67fb30c0bb
Marcin Lamek <46558308+fluffgit@users.noreply.github.com>
2021-10-05 20:42:53 +0200

    upload
    upload

---
+++ Client.java
@@ -0,0 +1,41 @@
+package w1_03102021.Bankomat;
+
+public class Client {
+
+    private String accountNumber;
+    private String pinNumber;
+
+    private double accountState;
+
+    public Client(String accountNumber, String pinNumber, double accountState) {
+        this.accountNumber = accountNumber;
+        this.pinNumber = pinNumber;
+        this.accountState = accountState;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getPinNumber() {
+        return pinNumber;
+    }
+
+    public void setPinNumber(String pinNumber) {
+        this.pinNumber = pinNumber;
+    }
+
+    public double getAccountState() {
+        return accountState;
+    }
+
+    public void setAccountState(double accountState) {
+        this.accountState = accountState;
+    }
+
+
+}
